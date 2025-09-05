476029c28cbec746f1f313f02115fce189c6cac8
Kay <80385994+twilightyear@users.noreply.github.com>
2025-09-05 10:41:05 +0900

    inline

---
+++ React JS/inline.js
@@ -0,0 +1,397 @@
+import React, {useState} from "react";
+export default (props) => {
+	const [input1, onChangeInput1] = useState('');
+	const [input2, onChangeInput2] = useState('');
+	const [input3, onChangeInput3] = useState('');
+	return (
+		<div 
+			style={{
+				display: "flex",
+				flexDirection: "column",
+				background: "#FFFFFF",
+			}}>
+			<div 
+				style={{
+					height: 1514,
+					alignSelf: "stretch",
+					display: "flex",
+					flexDirection: "column",
+					alignItems: "flex-start",
+					border: `1px solid #000000`,
+				}}>
+				<span 
+					style={{
+						color: "#00FFC3",
+						fontSize: 27,
+						fontWeight: "bold",
+						marginTop: 20,
+						marginBottom: 28,
+						marginLeft: 20,
+						marginRight: 20,
+					}} >
+					{"Pieta"}
+				</span>
+				<div 
+					style={{
+						alignSelf: "stretch",
+						display: "flex",
+						alignItems: "center",
+						marginBottom: 16,
+						marginLeft: 20,
+						marginRight: 20,
+						gap: 20,
+					}}>
+					<div 
+						style={{
+							flex: 1,
+							background: "#1A1A1AFA",
+							borderRadius: 12,
+							paddingTop: 20,
+							paddingBottom: 20,
+							boxShadow: "0px 0px 15px #00FFFF4D",
+						}}>
+						<div 
+							style={{
+								alignSelf: "stretch",
+								display: "flex",
+								flexDirection: "column",
+								alignItems: "flex-start",
+								background: "#E5E3DF",
+								paddingTop: 208,
+								paddingBottom: 101,
+								marginLeft: 20,
+								marginRight: 20,
+							}}>
+							<div 
+								style={{
+									alignSelf: "stretch",
+									display: "flex",
+									flexDirection: "column",
+									alignItems: "flex-end",
+									marginBottom: 41,
+									marginLeft: 432,
+								}}>
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/3qw5fk80_expires_30_days.png"} 
+									style={{
+										width: 1,
+										height: 1,
+										objectFit: "fill",
+									}}
+								/>
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/02zlim2c_expires_30_days.png"} 
+									style={{
+										width: 1,
+										height: 1,
+										objectFit: "fill",
+									}}
+								/>
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/phwuv9bu_expires_30_days.png"} 
+									style={{
+										width: 1,
+										height: 1,
+										marginBottom: 105,
+										objectFit: "fill",
+									}}
+								/>
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/ou97qgw6_expires_30_days.png"} 
+									style={{
+										width: 1,
+										height: 1,
+										marginBottom: 104,
+										objectFit: "fill",
+									}}
+								/>
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/nqipxti2_expires_30_days.png"} 
+									style={{
+										width: 1,
+										height: 1,
+										marginBottom: 104,
+										objectFit: "fill",
+									}}
+								/>
+								<img
+									src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/j0cpx0k2_expires_30_days.png"} 
+									style={{
+										width: 1,
+										height: 1,
+										objectFit: "fill",
+									}}
+								/>
+							</div>
+							<img
+								src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/x4hdl8pn_expires_30_days.png"} 
+								style={{
+									width: 1,
+									height: 1,
+									marginBottom: 194,
+									marginLeft: 539,
+									objectFit: "fill",
+								}}
+							/>
+							<div 
+								style={{
+									height: 1,
+									alignSelf: "stretch",
+									marginBottom: 90,
+									marginLeft: 1090,
+									marginRight: 109,
+								}}>
+							</div>
+							<div 
+								style={{
+									alignSelf: "stretch",
+									display: "flex",
+									flexDirection: "column",
+									alignItems: "flex-end",
+								}}>
+								<div 
+									style={{
+										display: "flex",
+										alignItems: "flex-start",
+										marginRight: 61,
+									}}>
+									<div 
+										style={{
+											flexShrink: 0,
+											display: "flex",
+											flexDirection: "column",
+											alignItems: "center",
+											paddingBottom: 1,
+											paddingLeft: 6,
+											paddingRight: 6,
+										}}>
+										<span 
+											style={{
+												color: "#000000",
+												fontSize: 10,
+											}} >
+											{"Keyboard shortcuts"}
+										</span>
+									</div>
+									<span 
+										style={{
+											color: "#000000",
+											fontSize: 10,
+											marginLeft: 6,
+											marginRight: 6,
+										}} >
+										{"Map data ©2025 TMap Mobility"}
+									</span>
+									<div 
+										style={{
+											flexShrink: 0,
+											display: "flex",
+											flexDirection: "column",
+											alignItems: "flex-start",
+											background: "#F5F5F5",
+											paddingLeft: 5,
+											paddingRight: 5,
+											marginLeft: 1,
+											marginRight: 1,
+										}}>
+										<span 
+											style={{
+												color: "#000000",
+												fontSize: 10,
+											}} >
+											{"Terms"}
+										</span>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div 
+						style={{
+							flexShrink: 0,
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "flex-start",
+							background: "#1A1A1A",
+							borderRadius: 16,
+							paddingTop: 39,
+							paddingBottom: 19,
+							boxShadow: "0px 0px 15px #00FFB433",
+						}}>
+						<span 
+							style={{
+								color: "#00FFC3",
+								fontSize: 18,
+								fontWeight: "bold",
+								marginTop: 1,
+								marginBottom: 19,
+								marginLeft: 20,
+								marginRight: 529,
+							}} >
+							{"부대 현황"}
+						</span>
+						<img
+							src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/5ovg247g_expires_30_days.png"} 
+							style={{
+								borderRadius: 16,
+								width: 580,
+								height: 580,
+								marginLeft: 20,
+								marginRight: 20,
+								objectFit: "fill",
+							}}
+						/>
+					</div>
+				</div>
+				<div 
+					style={{
+						alignSelf: "stretch",
+						display: "flex",
+						alignItems: "center",
+						marginBottom: 17,
+						marginLeft: 20,
+						marginRight: 20,
+						gap: 20,
+					}}>
+					<div 
+						style={{
+							flex: 1,
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "flex-start",
+							background: "#1A1A1A",
+							borderRadius: 16,
+							paddingTop: 39,
+							paddingBottom: 78,
+							boxShadow: "0px 0px 15px #00FFB433",
+						}}>
+						<span 
+							style={{
+								color: "#00FFC3",
+								fontSize: 18,
+								fontWeight: "bold",
+								marginTop: 1,
+								marginBottom: 20,
+								marginLeft: 20,
+								marginRight: 20,
+							}} >
+							{"병사 검색"}
+						</span>
+						<input
+							placeholder={"병사 이름을 입력하세요..."}
+							value={input1}
+							onChange={(event)=>onChangeInput1(event.target.value)}
+							style={{
+								color: "#757575",
+								fontSize: 14,
+								marginBottom: 19,
+								marginLeft: 20,
+								marginRight: 20,
+								alignSelf: "stretch",
+								background: "#111111",
+								borderRadius: 8,
+								border: "none",
+								paddingTop: 9,
+								paddingBottom: 9,
+								paddingLeft: 10,
+								paddingRight: 20,
+								boxShadow: "0px 0px 10px #00FFC34D",
+							}}
+						/>
+						<span 
+							style={{
+								color: "#FF4D4D",
+								fontSize: 18,
+								fontWeight: "bold",
+								marginTop: 2,
+								marginBottom: 21,
+								marginLeft: 20,
+								marginRight: 20,
+							}} >
+							{"긴급 알림"}
+						</span>
+						<input
+							placeholder={"⚠️ 위급 알림: 일병 임지민의 심정지"}
+							value={input2}
+							onChange={(event)=>onChangeInput2(event.target.value)}
+							style={{
+								color: "#FF6666",
+								fontSize: 16,
+								fontWeight: "bold",
+								marginBottom: 18,
+								marginLeft: 20,
+								marginRight: 20,
+								alignSelf: "stretch",
+								background: "#220000",
+								borderRadius: 10,
+								border: `1px solid #FF4444`,
+								paddingTop: 16,
+								paddingBottom: 16,
+								paddingLeft: 16,
+								paddingRight: 32,
+								boxShadow: "0px 0px 15px #FF464699",
+							}}
+						/>
+						<input
+							placeholder={"⚠️ 위급 알림: 일병 서광현의 무호흡 증상"}
+							value={input3}
+							onChange={(event)=>onChangeInput3(event.target.value)}
+							style={{
+								color: "#FF6666",
+								fontSize: 16,
+								fontWeight: "bold",
+								marginLeft: 20,
+								marginRight: 20,
+								alignSelf: "stretch",
+								background: "#220000",
+								borderRadius: 10,
+								border: `1px solid #FF4444`,
+								paddingTop: 16,
+								paddingBottom: 16,
+								paddingLeft: 16,
+								paddingRight: 32,
+								boxShadow: "0px 0px 15px #FF464699",
+							}}
+						/>
+					</div>
+					<div 
+						style={{
+							flexShrink: 0,
+							display: "flex",
+							flexDirection: "column",
+							alignItems: "flex-start",
+							background: "#1A1A1A",
+							borderRadius: 16,
+							paddingTop: 39,
+							paddingBottom: 20,
+							boxShadow: "0px 0px 15px #00FFB433",
+						}}>
+						<span 
+							style={{
+								color: "#00FFC3",
+								fontSize: 18,
+								fontWeight: "bold",
+								marginTop: 1,
+								marginBottom: 18,
+								marginLeft: 20,
+								marginRight: 534,
+							}} >
+							{"대시보드"}
+						</span>
+						<img
+							src={"https://storage.googleapis.com/tagjs-prod.appspot.com/v1/UG7ueamYkb/ba9dvnbk_expires_30_days.png"} 
+							style={{
+								borderRadius: 16,
+								width: 580,
+								height: 290,
+								marginLeft: 20,
+								marginRight: 20,
+								objectFit: "fill",
+							}}
+						/>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
